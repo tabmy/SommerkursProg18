1c7640fc98cffb2460cbe7546d7e9727da580941
AStrand94 <strand118@hotmail.com>
2018-07-06 18:49:13 +0200

    Nesten ferdig med oppgaver for dag 2

---
+++ dag_2/script.js
@@ -0,0 +1,8 @@
+function showExample(id) {
+    var element = document.getElementById(id);
+    if (element.style.display === "none") {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
+}
