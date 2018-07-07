a0af8a012265804ce25e5961012d5b48c2abcf4d
Tommy Abelsen <s237393@stud.hioa.no>
2018-07-07 12:20:59 +0200

    Console-logger beskrivelser av hendelser i webshop

--- dag_2/webshop.js
+++ dag_2/webshop.js
@@ -2,9 +2,11 @@ var items = [];
 
 function addItem(item) {
     items.push(item);
+    console.log("La til " + item);
 }
 
 function seeItems() {
+    console.log("Viser handlekurv");
     if (items.length === 0) {
         alert('Du har ikke valgt noe!');
     }else{
@@ -14,4 +16,5 @@ function seeItems() {
 
 function clearItems() {
     items = [];
+    console.log("Tømmer handlekurv");
 }
