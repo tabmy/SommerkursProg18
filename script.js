2d7d20de3ee2cd3ac7564b63a684a922845f8a9c
AStrand94 <strand118@hotmail.com>
2018-07-14 21:08:21 +0200

    Snart ferdig med dag 5

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function showExample(id) {
     var element = document.getElementById(id);
+    if (!element) {
+        console.log('\''+ id + '\' is not found');
+        return;
+    }
+
     if (element.style.display === "none") {
         element.style.display = "block";
     } else {
