422320efaa90ae65180f122128a4cc5c3875ecb4
AStrand94 <strand118@hotmail.com>
2018-07-14 12:32:17 +0200

    Ekstra oppgaver dag 2. Småfiksing. Startet smått på webshop dag 3.

---
+++ dag_5/webshop.js
@@ -0,0 +1,80 @@
+var items = [];
+var num = 0;
+
+function addItem(item) {
+    items.push(item);
+    cartAdd(item);
+    //cartShowName(item);
+}
+
+
+function seeItems() {
+    console.log("Viser handlekurv");
+    if (items.length === 0) {
+        alert('Du har ikke valgt noe!');
+    }else{
+        alert('Du har valgt: ' + items);
+    }
+}
+
+function clearItems() {
+    items = [];
+    console.log("Tømmer handlekurv");
+    document.getElementById('handlekurv').innerHTML = "";
+
+}
+
+function cartAdd(item) {
+    var kurv = document.getElementById('handlekurv');
+    var el = document.createElement('li');
+    el.classList.add('list-group-item');
+    var id = "element" + num++;
+    el.id = id;
+    el.appendChild(document.createTextNode(item + " "));
+
+     /*
+     Legger til en button til hvert listeelement
+     som sletter det valgte listeelementet.
+      */
+    var fjernBtn = document.createElement('button');
+    fjernBtn.appendChild(document.createTextNode('fjern'));
+    fjernBtn.onclick = function () {
+        this.parentNode.parentNode.removeChild(document.getElementById(id));
+        for (var i = items.length-1; i >=0; i--){
+            if (items[i] === item){
+                items.splice(i,1);
+                break;
+            }
+        }
+        console.log("Fjerner " + item);
+    };
+    fjernBtn.classList.add('btn','btn-danger');
+    el.appendChild(fjernBtn);
+
+
+    kurv.appendChild(el);
+    console.log('Legger til ' + item)
+}
+
+
+function cartShowName(item){
+    var kurv = document.getElementById('handlekurv');
+    kurv.innerHTML = item;
+}
+
+function getPrice() {
+    var numberOfItems = items.length;
+
+    $.ajax({
+       url: 'http://127.0.0.1:5000/calculate?price=50&items=' + numberOfItems,
+       type: 'GET',
+        success: function(result) {
+        showPrice(result);
+    }
+
+    });
+}
+
+function showPrice(price) {
+    document.getElementById('price').innerText = 'Totalpris = ' + price + ",- kr";
+}
